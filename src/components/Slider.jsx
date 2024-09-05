9830574156b4c2b82d0f96d36e91526550b0fd03
Usama dev <154013836+Usama-dev1@users.noreply.github.com>
2024-09-05 18:59:52 +0500

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,17 +1,16 @@
-import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the CSS for carousel
 import { useGetProductsQuery } from "../slices/productSlice";
 import Spinner from "./Spinner";
-
 const ProductCarousel = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { data = [], isLoading, error } = useGetProductsQuery();
+  console.log(data)
 
+  if (isLoading) return <Spinner />;
+  if (error) return <div>{error.data?.message || "An error occurred"}</div>;
 
   return (
     <div className="w-full flex justify-center">
-      {isLoading&&<Spinner/>}
-      {error&&error.data.message}
       <Carousel
         autoPlay
         infiniteLoop
@@ -19,16 +18,20 @@ const ProductCarousel = () => {
         showThumbs={false}
         showStatus={false}
         className="w-[40rem] m-2">
-        {products.map((product) => (
-          <div key={product.id}>
-            <img
-              alt={product.name || "Product image"}
-              src={product.image || "/default-image.jpg"} // Provide a default image if not available
-              className="w-full"
-            />
-            <p className="legend">{product.name}</p>
-          </div>
-        ))}
+        {data.length > 0 ? (
+          data.map((product) => (
+            <div key={product?._id}>
+              <img
+                alt={product?.name || "Product image"}
+                src={product?.image || "/default-image.jpg"} // Provide a default image if not available
+                className="w-full"
+              />
+              <p className="legend">{product?.name}</p>
+            </div>
+          ))
+        ) : (
+          <div>No products available</div>
+        )}
       </Carousel>
     </div>
   );
