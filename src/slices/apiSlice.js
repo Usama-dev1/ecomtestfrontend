61585e0ab9e1683329cbf24fdb678f4620c15cc0
Usama-dev1 <raja.usamah@gmail.com>
2024-09-06 15:18:25 +0500

    add slider and apislice mod

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -3,11 +3,7 @@ export const apiSlice = createApi({
   reducerPath: "apiSlice",
   tagTypes: ["product", "order", "user"],
   baseQuery: fetchBaseQuery({
-<<<<<<< HEAD
     baseUrl: "https://ecomtestrepoapi.vercel.app/api/",
-=======
-    baseUrl: "ecomtestrepoapi.vercel.app/api/",
->>>>>>> 9830574156b4c2b82d0f96d36e91526550b0fd03
     credentials: "include",
   }),
   endpoints: (builder) => ({}),
